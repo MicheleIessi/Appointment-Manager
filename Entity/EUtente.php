ade484568e1206fa6d5c317b2ca46c9aa5dcc943
Michele Iessi <iessimichele@gmail.com>
2015-08-20 20:05:05 +0200

    classe EUtente

--- Entity/EUtente.php
+++ Entity/EUtente.php
@@ -1,5 +1,5 @@
 <?php
-
+    require 'EPersona.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,6 +11,39 @@
  *
  * @author Michele
  */
-class EUtente {
-    //put your code here
+class EUtente extends EPersona {
+
+    protected $email;
+    protected $password;
+    protected $numID;
+    
+    public function __construct($n, $c, $dn, $cf, $s, $e, $p, $n) {
+        parent::__construct($n, $c, $dn, $cf, $s);
+        $this->setEmail($e);
+        $this->setPassword($p);
+        $this->setID($n);
+    }
+
+    public function setEmail($e) {
+        $pattern = "#^[a-zA-Z0-9]{1,20}@[a-zA-Z]{1,10}\.[a-zA-Z]{1,5}$#";
+        if (preg_match($pattern, $e) != 1) {
+            throw new Exception("E-Mail non valida", 1);
+        }
+        $this->email = $e;
+    }
+
+    public function setPassword($p) {
+        if(strlen($p) < 8) {
+            throw new Exception("Password troppo corta", 1);
+        }
+        if(strlent($p) > 20) {
+            throw new Exception("Password troppo lunga", 1);
+        }
+        $this->password = $p;
+    }
+
+    public function setID($n) {
+        
+    }
+
 }

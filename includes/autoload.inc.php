2cb7aba94c9fb17e1ece54908bd2d582b1e58023
Michele Iessi <iessimichele@gmail.com>
2015-08-30 18:33:32 +0200

    Autoload

--- includes/autoload.inc.php
+++ includes/autoload.inc.php
@@ -7,8 +7,8 @@
  */
 function __autoload($class_name) {
     switch ($class_name[0]) {
-        case 'V':
-            require_once ('View/'.$class_name.'.php');
+        case 'P':
+            require_once ('Presentation/'.$class_name.'.php');
             break;
         case 'F':
             require_once ('Foundation/'.$class_name.'.php');
@@ -18,10 +18,7 @@ function __autoload($class_name) {
             break;
         case 'C':
             require_once ('Controller/'.$class_name.'.php');
-            break;
-        case 'U':
-            require_once ('Foundation/Utility/'.$class_name.'.php');
-    }
+            }
 }
 
 

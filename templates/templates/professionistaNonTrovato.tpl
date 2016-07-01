f6c48366c47ec58c2db3786e00c181117824db34
Michele Iessi <iessimichele@gmail.com>
2016-07-01 12:11:05 +0200

    Fix sql calendario, aggiunta popup nell'inserimento appuntamento, rimozione appuntamento se il popup viene chiuso o viene selezionato annulla

---
+++ templates/templates/professionistaNonTrovato.tpl
@@ -0,0 +1,1 @@
+<div id="forbidden">Professionista non trovato</div>
---
+++ templates/templates_c/0f1946db70f0c3ad2e53268ada08d8114e8e0de3_0.file.professionistaNonTrovato.tpl.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version 3.1.27, created on 2016-06-29 22:10:24
+         compiled from "templates\templates\professionistaNonTrovato.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:2832857742b3009efe7_76099756%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0f1946db70f0c3ad2e53268ada08d8114e8e0de3' => 
+    array (
+      0 => 'templates\\templates\\professionistaNonTrovato.tpl',
+      1 => 1467231009,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2832857742b3009efe7_76099756',
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_57742b30530d74_92131316',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_57742b30530d74_92131316')) {
+function content_57742b30530d74_92131316 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '2832857742b3009efe7_76099756';
+?>
+<div id="forbidden">Professionista non trovato</div><?php }
+}
+?>

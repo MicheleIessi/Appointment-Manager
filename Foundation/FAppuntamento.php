f21530cd1a81a08c80c2ce98e72197427de619df
Michele Iessi <iessimichele@gmail.com>
2016-07-11 13:43:01 +0200

    php doc foundation e utility

--- Foundation/FAppuntamento.php
+++ Foundation/FAppuntamento.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * FAppuntamento si occupa di gestire appuntamenti sul database
+ * @package  Foundation
+ * @author   Michele Iessi
+ * @author   Davide Iessi
+ * @author   Andrea Pagliaro
+ * @access   public
+ */
 class FAppuntamento extends Fdb  {
 
     public function __construct() {
@@ -14,6 +22,20 @@ class FAppuntamento extends Fdb  {
         $this->old_keys;
     }
 
+    /** La funzione inserisciAppuntamenti serve ad inserire un appuntamento. Prima di chiamare il metodo di Fdb per
+     * l'inserimento, si occupa di effettuare alcuni controlli per verificare la correttezza dell'operazione che si
+     * vuole compiere.
+     * Nello specifico, per prima cosa chiama il metodo 'caricaConChiave' di Fdb. Se questo restituisce un valore diverso
+     * da false significa che l'utente ha già una prenotazione per quel dato giorno con quel dato professionista e
+     * l'appuntamento non viene inserito.
+     * Se questo conflitto non c'è, si passa a controllare che l'appuntamento che si vuole inserire non si accavalli con
+     * gli altri già presenti nell'agenda del professionista. Per farlo si comparano l'orario d'inizio e di fine
+     * dell'appuntamento che si vuole inserire con quelli degli appuntamenti già presenti. Se ci sono delle intersezioni
+     * l'inserimento non avviene.
+     * Se va tutto bene si procede con l'inserimento dell'appuntamento.
+     * @param EAppuntamento $app L'oggetto EAppuntamento che si vuole inserire nel database
+     * @return bool true se è stato inserito correttamente, false altrimenti
+     */
     public function inserisciAppuntamento(EAppuntamento $app) {
 
         /*
@@ -30,7 +52,7 @@ class FAppuntamento extends Fdb  {
         $chiaveDaCercare = 'IDP,IDC,data';
         try {
             if(parent::caricaConChiave($valoriDaCercare,$chiaveDaCercare) != false) {
-                return false; //sarebbe meglio mettere un return che simboleggia il risultato
+                return false;
             }
             else { // vuol dire che l'utente non ha già un appuntamento per il giorno selezionato
                 //controllo se altri utenti non hanno una prenotazione nell'orario scelto
@@ -73,6 +95,11 @@ class FAppuntamento extends Fdb  {
         }
     }
 
+    /** La funzione cancellaAppuntamento si occupa di eliminare delle ennuple dalla tabella appuntamento. Per farlo usa
+     * la funzione cancella di Fdb.
+     * @param $key int L'id dell'appuntamento che si vuole cancellare
+     * @return bool true se è stata cancellata con successo
+     */
     public function cancellaAppuntamento($key) {
         $this->setParametri();
         $valori[':IDApp']=$key;
@@ -87,13 +114,16 @@ class FAppuntamento extends Fdb  {
         }
     }
 
+    /** La funzione aggiornaAppuntamento aggiorna un appuntamento già presente nel database. L'oggetto Eappuntamento
+     * passato come parametro deve essere un appuntamento caricato precedentemente dal db, ovvero deve avere un
+     * IDAppuntamento corretto.
+     * @param EAppuntamento $app L'appuntamento aggiornato che si vuole caricare.
+     */
     public function aggiornaAppuntamento(EAppuntamento $app) {
         if($app->getIDAppuntamento() !== 0) {
             $this->setParametri();
             $valori = parent::cambiaChiaviArray($app->getArrayAttributi());
             $valori[':visita'] = $valori[':visita']->getNomeServizio();
-            var_dump($valori);
-            echo "<br>";
             try {
                 if (parent::aggiorna($valori) == 0) {
                     throw new PDOException("Impossibile modificare l'appuntamento.<br>");
@@ -107,9 +137,10 @@ class FAppuntamento extends Fdb  {
             echo "Appuntamento non presente nel database o non caricato correttamente.<br>";
     }
 
-    /**
-     * @param $key
-     * @return EAppuntamento
+    /** La funzione caricaAppuntamentoDaDb crea un oggetto EAppuntamento prendendo come parametro l'id dell'appuntamento
+     * che si vuole caricare.
+     * @param $key int L'id dell'appuntamento che si vuole caricare
+     * @return EAppuntamento L'appuntamento caricato
      */
     public function caricaAppuntamentoDaDb($key) {
         $this->setParametri();
@@ -134,15 +165,14 @@ class FAppuntamento extends Fdb  {
         parent::setParam($this->table,$this->attributi,$this->bind,$this->bind_key,$this->old_keys);
     }
 
+    /**
+     * La funzione setParametriInserimento setta i parametri di chiamata della funzione setParam di Fdb per controllare
+     * se per la data selezionata esiste già un appuntamento tra lo stesso cliente e lo stesso professionista.
+     */
     private function setParametriInserimento() {
-        $alt_attr="IDApp,IDP,IDC,data,orarioInizio,visita";
-        $alt_bind=":IDApp,:IDP,:IDC,:data,:orarioInizio,:visita";
-        $alt_keys=":IDP,:IDC,:data";
-        parent::setParam($this->table,$alt_attr,$alt_bind,$alt_keys,$this->old_keys);
+        $alt_attr = "IDApp,IDP,IDC,data,orarioInizio,visita";
+        $alt_bind = ":IDApp,:IDP,:IDC,:data,:orarioInizio,:visita";
+        $alt_keys = ":IDP,:IDC,:data";
+        parent::setParam($this->table, $alt_attr, $alt_bind, $alt_keys, $this->old_keys);
     }
-
-//    public function getLastID() {
-//        return parent::$last_id; // TODO: Change the autogenerated stub
-//    }
-
 }
